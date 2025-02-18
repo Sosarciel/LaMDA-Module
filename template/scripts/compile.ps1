b8d233026321d274e99addb423d7e1b62f3b1abc
zwa73 <ssfen2419@126.com>
2025-02-18 21:08:12 +0800

    add module

---
+++ template/scripts/compile.ps1
@@ -0,0 +1,7 @@
+﻿npm run expand-macro
+Write-Output 开始删除原dist
+Remove-Item -Recurse -Force dist
+Write-Output 开始编译
+tsc -p tsconfig.compile.json
+tsc-alias -p tsconfig.compile.json
+scripts/build-schema
